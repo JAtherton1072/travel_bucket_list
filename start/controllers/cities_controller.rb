a165d216e72a46e1a13ba78954faecf7dd07e409
JAtherton <atherton374@btinternet.com>
2019-09-30 16:26:41 +0100

    browser add sight, city, country

--- start/controllers/cities_controller.rb
+++ start/controllers/cities_controller.rb
@@ -9,3 +9,24 @@
     @cities = City.all
     erb( :"cities/index" )
   end
+
+
+
+
+  # NEW
+
+
+  get '/cities/new' do
+    @countries = Country.all
+    erb( :"cities/new" )
+  end
+
+
+  # CREATE
+
+  post '/cities' do
+    City.new(params).save
+    redirect '/cities'
+
+
+  end
--- start/controllers/sights_controller.rb
+++ start/controllers/sights_controller.rb
@@ -9,3 +9,24 @@
    @sights = Sight.all
    erb( :"sights/index" )
  end
+
+
+
+
+ # NEW
+
+
+ get '/sights/new' do
+   @cities = City.all
+   erb( :"sights/new" )
+ end
+
+
+ # CREATE
+
+ post '/sights' do
+   Sight.new(params).save
+   redirect '/sights'
+
+
+ end
